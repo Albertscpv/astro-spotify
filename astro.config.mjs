248c49f82362f58714b5af2c60057cdaf99cf6ee
Albert Dev <125626643+Albertscpv@users.noreply.github.com>
2024-11-26 01:32:06 +0000

    config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), svelte()],
-  output: "server",
+  integrations: [tailwind(), react(), svelte(), vercel()],
+  output: "static",
   adapter: vercel()
 });
